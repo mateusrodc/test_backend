d8049c90c80ac9895c46af6eaa6dff6e23100f9c
Mateus Rodrigues <mcmateus070@gmail.com>
2021-03-08 23:21:38 -0300

    resolvendo conexao com o SQL Server

---
+++ Migrations/20210309015339_InitialCreate.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Computers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Marca = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    PlacaMae = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    MemoriaRam = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Hd = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    VelocidadeProcessador = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Foto = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Computers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Usuario = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Senha = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Computers");
+
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
